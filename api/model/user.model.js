4eb5cee11a067b059c5306d0c93bcd14647d56c3
dhruv <dhruvgohil1624@gmail.com>
2023-12-27 19:08:12 +0530

    created user model

---
+++ api/model/user.model.js
@@ -0,0 +1,21 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+        username : {
+            type : String,
+            required : true,
+            unique : true,
+        },
+
+        password : {
+            type : String,
+            required : true,
+            minlength : 8,
+        }
+    },{timestamps:true});
+
+
+const user = mongoose.model('User',userSchema);
+
+export default user;

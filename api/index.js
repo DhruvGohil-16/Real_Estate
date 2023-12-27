44052bbb3756bee2e923db34ad15cbb9b5eee864
dhruv <dhruvgohil1624@gmail.com>
2023-12-27 17:55:28 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 import express from 'express';  //make type in package.json as "module"
 // const express = require('express');
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app = express();
 const port = 1624;
+mongoose.connect(process.env.MONGO)
+        .then(() => console.log("--Connected to real-estate database--"))
+        .catch((err) => {console.log(err);})
 
 app.listen(port, ()=>{
-    console.log("Server running on port 1624!!!!");
+    console.log("--Server running on port 1624--");
 });
